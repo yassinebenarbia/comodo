11f10259e0ca74a76723d508d60cc4ed2e0a748e
yassine <yassinebenarbia980@gmail.com>
2024-03-18 06:04:33 +0100

    feat: controlling notification behavior from the config file

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
-use std::{fs, time::Duration};
+use std::{fs, time::Duration, path::{Path, PathBuf}};
 // use serde::{Serialize, Deserialize, de::{self, Visitor}};
-use serde::{ser::{SerializeStruct, Serializer}, Deserialize, de::Visitor, Serialize, Deserializer };
+use serde::{ser::{SerializeStruct, Serializer}, Deserialize, Serialize, Deserializer,  de::Error};
 
 #[derive(Debug, Clone, Copy)]
 pub struct RGB(pub u8, pub u8, pub u8);
@@ -10,13 +10,18 @@ pub struct RGB(pub u8, pub u8, pub u8);
 //                        Serialization
 // Toml Representation <------------------- Config Structure Object
 
-struct ComodoroDefaults;
-impl ComodoroDefaults{
-    
-    fn focus()->Duration{ Duration::from_secs(1500) }
-    fn rest()->Duration{ Duration::from_secs(300) }
-    fn big_rest()->Duration{ Duration::from_secs(900) }
-    fn iterations()->u8{4}
+struct ComodoDefaults;
+impl ComodoDefaults{
+    pub fn focus()->Duration{ Duration::from_secs(1500) }
+    pub fn rest()->Duration{ Duration::from_secs(300) }
+    pub fn big_rest()->Duration{ Duration::from_secs(900) }
+    pub fn iterations()->u8{4}
+    pub fn popup_notification() -> bool { true }
+    pub fn sound_notification() -> bool { false }
+    pub fn focus_notification_banner() -> String { String::from("Focus Time!") }
+    pub fn rest_notification_banner() -> String { String::from("Resting Time!") }
+    pub fn focus_notification_path() -> Option<Box<PathBuf>> { None }
+    pub fn rest_notification_path() -> Option<Box<PathBuf>> { None }
 }
 
 fn from_str_to_duration(s: &str) -> Result<Duration, std::num::ParseIntError> {
@@ -26,10 +31,32 @@ fn from_str_to_duration(s: &str) -> Result<Duration, std::num::ParseIntError> {
     Ok(Duration::from_secs(minutes * 60 + seconds))
 }
 
+// TODO: handle this more properly, mainly 'config does not exist!' error situation
+fn deserialize_path<'de, D>(d: D) -> Result<Option<Box<PathBuf>>, D::Error>
+    where D: Deserializer<'de>
+{
+    let s: Option<String> = Option::deserialize(d)?;
+    match s {
+        Some(value) => {
+            let path = Path::new(value.as_str()).to_owned();
+            if path.exists() {
+                return Ok(Some(Box::new(path)))
+            }else {
+                return Ok(None)
+            };
+        },
+        None => return Ok(None),
+    }
+}
+
+// Substitute the # withthe thing
+fn deserialize_banner<'de, D>(d: D) -> Result<String, D::Error>
+    where D: Deserializer<'de>
+{todo!()}
+
 fn deserialize_time<'de, D>(d: D) -> Result<Duration, D::Error>
     where D: Deserializer<'de>
 {
-    println!("hi");
     let s: Option<String> = Option::deserialize(d)?;
     match s {
         Some(value) => {
@@ -40,14 +67,29 @@ fn deserialize_time<'de, D>(d: D) -> Result<Duration, D::Error>
 }
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct Comodoro{
+pub struct Comodo{
     pub iterations: u8,
     #[serde(deserialize_with = "deserialize_time")]
     pub focus: Duration,
     #[serde(deserialize_with = "deserialize_time")]
     pub rest: Duration,
-    #[serde(deserialize_with = "deserialize_time", rename="big-rest")]
+    #[serde(deserialize_with = "deserialize_time")]
     pub big_rest: Duration,
+
+    #[serde(default = "ComodoDefaults::popup_notification")]
+    pub popup_notification: bool,
+    #[serde(default = "ComodoDefaults::sound_notification")]
+    pub sound_notification: bool,
+
+    #[serde(default = "ComodoDefaults::focus_notification_banner")]
+    pub focus_notification_banner: String,
+    #[serde(default = "ComodoDefaults::rest_notification_banner")]
+    pub rest_notification_banner: String,
+
+    #[serde(deserialize_with = "deserialize_path", default = "ComodoDefaults::focus_notification_path")]
+    pub focus_audio_notification_path: Option<Box<PathBuf>>,
+    #[serde(deserialize_with = "deserialize_path", default = "ComodoDefaults::rest_notification_path")]
+    pub rest_audio_notification_path: Option<Box<PathBuf>>,
 }
 
 pub fn from_duration_to_str(duration: Duration) -> String{
@@ -68,11 +110,11 @@ pub fn from_duration_to_str(duration: Duration) -> String{
     format!("{}:{}", sminutes, sseconds)
 }
 
-impl Serialize for Comodoro {
+impl Serialize for Comodo {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
-            let mut s = serializer.serialize_struct("Comodoro", 4)?;
+            let mut s = serializer.serialize_struct("Comodo", 4)?;
             s.serialize_field("iterations", &self.iterations)?;
             s.serialize_field("focus", &from_duration_to_str(self.focus))?;
             s.serialize_field("rest", &from_duration_to_str(self.rest))?;
@@ -84,8 +126,8 @@ impl Serialize for Comodoro {
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Config {
-    #[serde(rename="comodoro")]
-    pub comodoro: Comodoro,
+    #[serde(rename="comodo")]
+    pub comodo: Comodo,
 }
 
 impl Config {
@@ -98,12 +140,14 @@ impl Config {
         // stands for string config
         let sconfig = fs::read_to_string(path).unwrap();
 
-        let comodoro: Comodoro = toml::de::from_str(sconfig.as_str()).unwrap();
+        let comodo: Comodo = toml::de::from_str(sconfig.as_str()).unwrap();
 
         Config {
-            comodoro
+            comodo
         }
     }
+
+    pub fn from_stream_string(input: String) -> Self {todo!()}
 
 }
 
@@ -114,12 +158,12 @@ mod test{
     use crate::config::Config;
     use toml;
 
-    use super::Comodoro;
+    use super::Comodo;
 
     #[test]
     fn contains_test() {
         let conf: Config = toml::de::from_str(r#"
-            [comodoro]
+            [comodo]
             iterations = 5
             focus = "25:00"
             rest = "05:00"
@@ -130,11 +174,10 @@ mod test{
 
     #[test]
     fn file_test() {
-        let config = "./comodoro.toml";
+        let config = "./comodo.toml";
         let content = fs::read_to_string(config).unwrap();
         println!("content: {}", content);
-        let comodoro: Config = toml::de::from_str(content.as_str()).unwrap();
-        println!("comodoro: {:#?}", comodoro);
+        let comodo: Config = toml::de::from_str(content.as_str()).unwrap();
+        println!("comodo: {:#?}", comodo);
     }
 }
-
